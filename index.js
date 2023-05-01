3b7ea08466a77fa49cec8c8484b6d2b71287b9dd
kailahk <kailahkorsh@me.com>
2023-04-30 19:55:18 -0700

    styles day/night mode button, languages text on home page

--- index.js
+++ index.js
@@ -5,13 +5,20 @@ const kkImg = document.getElementsByClassName('kk-img')
 modeBtn.addEventListener('click', handleModeChange)
 
 function handleModeChange(event) {
-    if (event.target.innerHTML === 'Day') {
+    if (event.target.innerHTML === 'DAY') {
         document.body.style.backgroundImage = "url('https://i.imgur.com/mSODwDU.jpg')"
-        modeBtn.innerHTML = 'Night'
+        modeBtn.innerHTML = 'NIGHT'
         kkImg[0].src = 'https://i.imgur.com/kRorks5.png'
+        document.getElementById('home-headline-and-description').setAttribute('class', 'background-readability')
+        document.getElementById('mode-btn').style.color = '#4E6683'
+        document.getElementById('mode-btn').style.borderColor = '#4E6683'
     } else {
         document.body.style.backgroundImage = "url('https://i.imgur.com/cFs1PHv.jpg')"
-        modeBtn.innerHTML = 'Day'
+        modeBtn.innerHTML = 'DAY'
         kkImg[0].src = 'https://i.imgur.com/yBD4epQ.png'
+        console.log(document.getElementsByClassName('home-headline-and-description')[0])
+        document.getElementById('home-headline-and-description').removeAttribute('class', 'background-readability')
+        document.getElementById('mode-btn').style.color = '#F6F3E4'
+        document.getElementById('mode-btn').style.borderColor = '#F6F3E4'
     }
 }
