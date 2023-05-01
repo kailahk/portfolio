6c9896becd53ea70e6d7d04a93eca940fccddb6f
kailahk <kailahkorsh@me.com>
2023-04-30 17:27:39 -0700

    kk logo changes upon clicking 'day' or 'night'

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const modeBtn = document.getElementById('mode-btn')
 const bodyEl = document.getElementsByClassName('home-body')
+const kkImg = document.getElementsByClassName('kk-img')
 
 modeBtn.addEventListener('click', handleModeChange)
 
@@ -7,8 +8,10 @@ function handleModeChange(event) {
     if (event.target.innerHTML === 'Day') {
         document.body.style.backgroundImage = "url('https://i.imgur.com/mSODwDU.jpg')"
         modeBtn.innerHTML = 'Night'
+        kkImg[0].src = 'https://i.imgur.com/kRorks5.png'
     } else {
         document.body.style.backgroundImage = "url('https://i.imgur.com/cFs1PHv.jpg')"
         modeBtn.innerHTML = 'Day'
+        kkImg[0].src = 'https://i.imgur.com/yBD4epQ.png'
     }
 }
